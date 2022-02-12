a6ca16c491d031479137ca550ce448a5a4ead89a
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2022-02-13 07:55:58 +1300

    Initial Rest API

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+const port = process.env.PORT || 5000
+
+connectDB();
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
